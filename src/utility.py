91e59c7a42dc58979a2bb12697337b8ed19c39be
Roman Zberg <roman.zberg@stud.hslu.ch>
2025-05-08 22:57:23 +0200

    push changes

--- src/utility.py
+++ src/utility.py
@@ -20,7 +20,7 @@ def load_image(image_path) -> np.ndarray:
 
 
 def get_color_pallete(color_to_number_dict):
-    fig, axs = plt.subplots(1, len(color_to_number_dict), figsize=(len(color_to_number_dict) * 3, 3))
+    fig, axs = plt.subplots(1, len(color_to_number_dict), figsize=(len(color_to_number_dict) * 3, 4))
 
     for ax, (rgb_255, label_number) in zip(axs, color_to_number_dict.items()):
         rgb_normalized = tuple(c / 255 for c in rgb_255)
@@ -28,6 +28,8 @@ def get_color_pallete(color_to_number_dict):
         img = np.full((10, 10, 3), rgb_normalized)
         ax.imshow(img)
         ax.axis('off')
-        ax.set_title(f"{label_number} RGB: {[int(value) for value in rgb_255]}")
+        ax.set_title(f"{label_number}: RGB: {[int(value) for value in rgb_255]}")
+
+    fig.tight_layout()
 
     return fig, axs
